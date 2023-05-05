f0e0ba4bc86fc3a5535a79b6f175a4d62a160a92
daj10 <61434035+daj10@users.noreply.github.com>
2023-05-05 19:22:28 +0200

    home empty url

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('posts/detail/<int:post_id>/', views.post_detail, name='post-detail'),
     path('posts/detail/<int:post_id>/<str:message>/', views.post_detail, name='post-detail-message'),
     path('about/', views.about, name='about'),
+    path('', views.post_list, name='index'),
 ]
